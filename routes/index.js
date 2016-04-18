043e8b91b01345eadf9688ba58eeee252b284814
kaileylap <kaileylap@megadome.org>
2016-04-18 18:48:22 -0500

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var weather = require('weather-js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+	  if(err){res.render('index', { title: 'Express' });}
+	 
+	  //console.log(JSON.stringify(result, null, 2));
+
+	  res.render('index', { title: result[0].location.name });
+	  
+	});
+
+  
 });
 
 module.exports = router;

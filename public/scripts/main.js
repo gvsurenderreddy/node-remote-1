78467368cedd9c2b732de81c32229e52882c8455
kaileylap <kaileylap@megadome.org>
2016-04-25 19:50:24 -0500

    no message

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,9 +2,64 @@ $(document).ready(function(){
 
 	var socket = io();
 
+	var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+
+	function onYouTubeIframeAPIReady() {
+	  player = new YT.Player('player', {
+	    height: '390',
+	    width: '640',
+	    videoId: 'M7lc1UVf-VE',
+	    events: {
+	      'onReady': onPlayerReady,
+	      'onStateChange': onPlayerStateChange
+	    }
+	  });
+	}
+
+	function onReady(){}
+
+	function onStateChange(){}
+
+
 	socket.on('play', function(data){
-		console.log(data);
+        player.loadVideoById(data, 5, "large");
 	});
+
+	// // 2. This code loads the IFrame Player API code asynchronously.
+ //        var tag = document.createElement('script');
+ 
+ //        tag.src = "https://www.youtube.com/iframe_api";
+ //        var firstScriptTag = document.getElementsByTagName('script')[0];
+ //        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+ 
+ //        // 3. This function creates an <iframe> (and YouTube player)
+ //        //    after the API code downloads.
+ //        var player;
+ 
+ //        function onYouTubeIframeAPIReady() {
+ //            player = new YT.Player('player', {
+ //                height: $(document).height(),
+ //                width: $(document).width(),
+ //                videoId: 'KkOxeKA2WNE',
+ //                events: {
+ //                    'onReady': onPlayerReady
+ //                }
+ //            });
+ //        }
+ 
+ 
+ //        // 4. The API will call this function when the video player is ready.
+ //        function onPlayerReady(event) {
+ //            event.target.playVideo();
+ //        }
 
 
 	
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,30 @@ var weather = require('weather-js');
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync(__dirname+'/../config.json', 'UTF-8'));
 
+//get lan ip
+var os = require('os');
+
+//get lan ip
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+var lanIP = addresses[0];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
 	weather.find({search: config.zip, degreeType: 'F'}, function(err, result) {
 	  if(err){res.send("There was an error");}
 
-	  res.render('index', { "cityName": result[0].location.name, "cityTemp": result[0].current.temperature, "cityImg": result[0].current.imageUrl });
+	  res.render('index', { "lanIP": lanIP, "cityName": result[0].location.name, "cityTemp": result[0].current.temperature, "cityImg": result[0].current.imageUrl });
 	  
 	});
 

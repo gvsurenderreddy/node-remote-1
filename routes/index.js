8a24f2fb352f5bc7d15fc092a2ed2b194d5a8634
conductor609 <mreyebrow@gmail.com>
2016-04-27 20:21:33 -0500

    no message

--- routes/index.js
+++ routes/index.js
@@ -25,15 +25,23 @@ var lanIP = addresses[0];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-	weather.find({search: config.zip, degreeType: 'F'}, function(err, result) {
-	  if(err){res.send("There was an error");}
-
-	  res.render('index', { "lanIP": lanIP, "cityName": result[0].location.name, "cityTemp": result[0].current.temperature, "cityImg": result[0].current.imageUrl });
-	  
-	});
-
-  
+ 
+  weather.find({search: 'Whitewater, WI', degreeType: 'F'}, function (err, result) {
+    if (err) {
+      res.send("There was an error");
+    }
+ 
+    res.render('index', {
+      "cityName": result[0].location.name, "cityTemp": result[0].current.temperature,
+      "cityImg": result[0].current.imageUrl, "feelsLike": result[0].current.feelslike,
+      "wind": result[0].current.winddisplay, "humidity": result[0].current.humidity,
+      "day2": result[0].forecast[2].day, "day2Low": result[0].forecast[2].low, "day2High": result[0].forecast[2].high,
+      "day2Precip": result[0].forecast[2].precip, "day3": result[0].forecast[3].day, "day3Low": result[0].forecast[3].low,
+      "day3High": result[0].forecast[3].high, "day3Precip": result[0].forecast[3].precip, "day4": result[0].forecast[4].day,
+      "day4Low": result[0].forecast[4].low, "day4High": result[0].forecast[4].high,
+      "day4Precip": result[0].forecast[4].precip,
+      "lanIP":lanIP});
+ 
+  });
 });
-
 module.exports = router;
